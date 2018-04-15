96a63f4ee318b8ab4d4b51b115485bfdd1fd5268
Michal Stabrawa <mistaaqua@gmail.com>
2018-04-15 22:14:55 +0200

    RWD col-sm

--- js/main.js
+++ js/main.js
@@ -2,4 +2,30 @@ $(document).ready(function(){
     $('#mobile-menu').click(function(){
         $('.nav-mobile-ul').toggle();
     })
+    
+    
+    $('#scroll-down').on('click', function () {
+    $('html, body').animate({
+        scrollTop: $('#aboutUs').offset().top
+    }, 1000);
+});
+    
+    /*myBtn */
+    
+    window.onscroll = function () {
+    scrollFunction()
+};
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+    
+    $('#myBtn').click(function(){
+        $('html, body').animate({
+            scrollTop: $('header').offset().top
+        },1000);
+    })
 })
